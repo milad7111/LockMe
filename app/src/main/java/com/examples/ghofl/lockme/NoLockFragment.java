0b836833c9051ec7a3f0a0789acd2550f552669b
Unknown <milad7111@chmail.ir>
2018-04-25 01:11:23 +0430

    Get List of Available Wifi Networks from ESP8266

--- app/src/main/java/com/examples/ghofl/lockme/NoLockFragment.java
+++ app/src/main/java/com/examples/ghofl/lockme/NoLockFragment.java
@@ -4,16 +4,26 @@ import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class NoLockFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     private Button _btn_add_first_lock;
+    private TextView _txv_no_lock;
 
     public NoLockFragment() {
     }
@@ -27,6 +37,7 @@ public class NoLockFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.layout_fragment_no_lock, container, false);
 
         _btn_add_first_lock = rootView.findViewById(R.id.btn_add_first_lock);
+        _txv_no_lock = rootView.findViewById(R.id.txv_no_lock);
 
         return rootView;
     }
@@ -52,15 +63,36 @@ public class NoLockFragment extends Fragment {
 
     public void onStart() {
         super.onStart();
+
+        setContentOfTextViewDependsOnInternetConnection();
+
         _btn_add_first_lock.setOnClickListener(new OnClickListener() {
             public void onClick(View view) {
-                ((LockActivity)getActivity()).LoadFragment(new AddLockFragment(), getString(R.string.fragment_add_lock_fragment));
+                ((LockActivity) getActivity()).LoadFragment(new AddLockFragment(), getString(R.string.fragment_add_lock_fragment));
             }
         });
     }
 
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri var1);
+    }
+
+    private void setContentOfTextViewDependsOnInternetConnection() {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(getActivity().getBaseContext());
+        String url = getString(R.string.dummy_http_url);
+        StringRequest MyStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener() {
+            @Override
+            public void onResponse(Object response) {
+                Log.e(getTag(), response.toString() + getString(R.string.log_connected_to_internet));
+                _txv_no_lock.setText(R.string.there_is_no_lock);
+            }
+        }, new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+                _txv_no_lock.setText(getString(R.string.connect_to_internet));
+                Log.e(getTag(), error.getMessage());
+            }
+        });
+        MyRequestQueue.add(MyStringRequest);
     }
 }
 
